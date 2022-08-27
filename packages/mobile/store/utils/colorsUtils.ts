0b8e08ab6f917c3c1c2f92b9b5d437d325fdfc7a
Rico Ardiya <ricoardiya@yahoo.com>
2022-08-28 03:38:37 +0800

    feat(game): add theme (#21)

--- packages/mobile/store/utils/colorsUtils.ts
+++ packages/mobile/store/utils/colorsUtils.ts
@@ -1,37 +1,38 @@
 import randomUtils from './randomUtils'
+import colors from '../../config/colors'
 
 const NUMBER_OF_COLORS = 15
-const NUMBER_OF_RED = 1
-const NUMBER_OF_BLUE = 3
-const RED = 'red'
+const NUMBER_OF_TARGET = 1
+const NUMBER_OF_DECOY = 3
+const DECOY = colors.RED_CIRCLE
 const WHITE = 'white'
-const BLUE = 'blue'
+const TARGET = colors.GREEN_CIRCLE
 
 const generateColors = (numberOfColors: number): Array<string> => {
   let colors: Array<string> = []
-  let red = NUMBER_OF_RED
-  let blue = NUMBER_OF_BLUE
+  let target = NUMBER_OF_TARGET
+  let decoy = NUMBER_OF_DECOY
   for (let i = 0; i < numberOfColors; i++) {
     // Make sure red is always in the grid
     if (i == numberOfColors - 1) {
-      if (red > 0) {
-        colors = [...colors, RED]
-        red = red - 1
+      if (target > 0) {
+        colors = [...colors, TARGET]
+        target = target - 1
         continue
       }
     }
     const color = randomUtils.getRandomInt(10)
     if (color === 0) {
-      if (red > 0) {
-        colors = [...colors, RED]
-        red = red - 1
+      if (target > 0) {
+        colors = [...colors, TARGET]
+        target = target - 1
         continue
       }
     }
     if (color === 1) {
-      if (blue > 0) {
-        colors = [...colors, BLUE]
-        blue = blue - 1
+      if (decoy > 0) {
+        colors = [...colors, DECOY]
+        decoy = decoy - 1
         continue
       }
     }
@@ -43,7 +44,7 @@ const generateColors = (numberOfColors: number): Array<string> => {
 export default {
   generateColors,
   NUMBER_OF_COLORS,
-  RED,
-  BLUE,
+  DECOY,
+  TARGET,
   WHITE
 }
