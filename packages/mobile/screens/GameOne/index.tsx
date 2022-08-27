68f5c6619f0af63a10e6a8a8b40a0c51151fc736
ricoardiya <ricoardiya@yahoo.com>
2022-08-27 14:50:25 +0800

    Add state

--- packages/mobile/screens/GameOne/index.tsx
+++ packages/mobile/screens/GameOne/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View } from '../../components/Themed';
 import styles from './index.style';
 import TimeBar from "../../components/TimeBar";
 import Grid from "./Grid";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {Text} from "react-native";
+import {startGameTwoAction, startTimerAction} from "../../store/actions/gameTwoActions";
 
 export default function GameOne() {
-    return (
-        <View style={styles.container}>
+  const gameOneState = useAppSelector((state) => state.gameOneReducers)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(startGameTwoAction())
+    dispatch(startTimerAction())
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      {gameOneState.isGameRunning &&
+        (
+          <>
             <TimeBar />
             <Grid />
-        </View>
-    )
+          </>
+        )
+      }
+      {!gameOneState.isGameRunning &&
+        <>
+          <Text>your score is {gameOneState.score}</Text>
+        </>
+      }
+    </View>
+  )
 }
