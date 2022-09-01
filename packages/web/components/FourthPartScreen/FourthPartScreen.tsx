c7a80872ae413e33821603ed5ce446775d7b59d5
ricoardiya <ricoardiya@yahoo.com>
2022-09-01 21:45:57 +0800

    Visit app button link to production mobile

--- packages/web/components/FourthPartScreen/FourthPartScreen.tsx
+++ packages/web/components/FourthPartScreen/FourthPartScreen.tsx
@@ -19,6 +19,12 @@ const FourthPartScreen = () => {
       window.removeEventListener('resize', updateDimensions)
     }
   }, [])
+
+  // this should be in configuration file
+  const onVisitApp = () => {
+    window.open('https://soba-up-mobile.vercel.app', '_blank')
+  }
+
   return (
     <div className={styles.backgroundContainer}>
       <div className={styles.buttonContainer}>
@@ -28,13 +34,20 @@ const FourthPartScreen = () => {
             {windowWidth > 1401 ? (
               <>
                 <div className={styles.hr2}></div>
-                <Button sx={{ color: 'black', pr: 2, pl: 2, m: 0.5 }}>
+                <Button
+                  sx={{ color: 'black', pr: 2, pl: 2, m: 0.5 }}
+                  onClick={onVisitApp}
+                >
                   <h3>Visit App</h3>
                 </Button>
               </>
             ) : (
               <>
-                <Button sx={{ borderColor: 'white' }} variant="outlined">
+                <Button
+                  sx={{ borderColor: 'white' }}
+                  variant="outlined"
+                  onClick={onVisitApp}
+                >
                   <h3>Visit App</h3>
                 </Button>
               </>
